d33bb7eab64cf8d71be8996d74ff7ac42f8ec794
Parukc <carlosasp-13@hotmail.com>
2024-07-09 19:25:01 -0500

    Ejercicio_3

---
+++ Ejercicio_3.py
@@ -0,0 +1,7 @@
+#Ejercicio 3: Escribe un programa que determine si un número introducido por el usuario es par o impar
+num1 = int(input("Ingrese un numero"))
+
+if num1 % 2 == 0:
+  print("El numero es par ")
+else:
+  print("El numeo es impar ")
