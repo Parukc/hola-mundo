dcea845d1139f3f0ecb50d280f38ba882dff87ac
Parukc <carlosasp-13@hotmail.com>
2024-07-09 19:33:24 -0500

    Ejercicio_7

---
+++ Ejercicio_7.py
@@ -0,0 +1,6 @@
+#Ejercicio 7: Escribe un programa que convierta una temperatura dada en grados Celsius a grados Fahrenheit.
+celcius = int(input("Introduzca un valor de temperatura Celcius"))
+
+fahrenheit = (celcius * 9/5) + 32
+
+print(f"los grados celcius {celcius} transformados en fahrenheit son {fahrenheit}")
