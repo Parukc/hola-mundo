1737c0f29619978e802a8148f5c3122d6f2c6f51
Parukc <carlosasp-13@hotmail.com>
2024-07-09 19:32:28 -0500

    Ejercicio_6

---
+++ Ejercicio_6.py
@@ -0,0 +1,6 @@
+#Ejercicio 6: Escribe un programa que calcule el área de un círculo a partir de su radio, ingresado por el usuario.
+radio = int(input("Ingrese el valor del radio"))
+
+area = 3.1416 * radio**2
+
+print("El radio del circulo es", area)
