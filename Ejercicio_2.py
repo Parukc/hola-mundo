e51550a2fcc4794b2c8966bb35c9434487b50350
Parukc <carlosasp-13@hotmail.com>
2024-07-09 19:23:40 -0500

    Ejercicio_2

---
+++ Ejercicio_2.py
@@ -0,0 +1,7 @@
+#Ejercicio 2:  Escribe un programa que pida al usuario dos números y los sume.
+num1 = int(input("Ingrese un numero"))
+num2 = int(input("Ingrese el segundo numero"))
+
+num3 = num1 + num2
+
+print(num3)
